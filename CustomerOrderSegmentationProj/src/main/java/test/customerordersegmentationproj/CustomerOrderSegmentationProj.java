86d4f75945107e7af660e694aae8d60be022ec2e
lurkinliev <iliakarol123@gmail.com>
2024-10-12 23:01:06 +0800

    inserted int and println

--- CustomerOrderSegmentationProj/src/main/java/test/customerordersegmentationproj/CustomerOrderSegmentationProj.java
+++ CustomerOrderSegmentationProj/src/main/java/test/customerordersegmentationproj/CustomerOrderSegmentationProj.java
@@ -13,6 +13,10 @@ package test.customerordersegmentationproj;
 public class CustomerOrderSegmentationProj {
 
     public static void main(String[] args) {
+        int x = 1, y = 2, z = 3;
+
         System.out.println("Hello World!");
+        System.out.println(x + y + z);
+
     }
 }
