f4243d92a23b67a27526ff7ea9f7da78824e8252
EnigmaticPizza <alennkaezzarmarcos@gmail.com>
2024-10-12 22:51:18 +0800

    Test

---
+++ CustomerOrderSegmentationProj/src/main/java/test/customerordersegmentationproj/CustomerOrderSegmentationProj.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package test.customerordersegmentationproj;
+
+/**
+ *
+ * check check test if this works
+ * 
+ * 
+ */
+public class CustomerOrderSegmentationProj {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
